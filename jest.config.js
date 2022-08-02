c2ad9285b9f657bcb0dfb701ff4f1ab408a07b70
superblocksadmin <admin@superblockshq.com>
2022-08-02 18:07:06 +0000

    Releasing workflow version 0.1891.0

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,15 @@
 module.exports = {
-  roots: ["<rootDir>/"],
+  roots: ['<rootDir>/'],
   transform: {
-    "^.+\\.(png|ts|tsx)$": "ts-jest",
+    '^.+\\.(png|ts|tsx)$': 'ts-jest'
   },
-  testRegex: "(/__tests__/.*|(\\.)(test|spec))\\.tsx?$",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node", "css"],
-  moduleDirectories: ["node_modules", "src"],
-  transformIgnorePatterns: ["<rootDir>/node_modules/"],
+  testRegex: '(/__tests__/.*|(\\.)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node', 'css'],
+  moduleDirectories: ['node_modules', 'src'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
   moduleNameMapper: {
-    "\\.(css|less)$": "<rootDir>/test/__mocks__/styleMock.js",
-    "\\.svg$": "<rootDir>/test/__mocks__/svgMock.js",
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/test/__mocks__/fileMock.js",
-  },
+    '\\.(css|less)$': '<rootDir>/test/__mocks__/styleMock.js',
+    '\\.svg$': '<rootDir>/test/__mocks__/svgMock.js',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/test/__mocks__/fileMock.js'
+  }
 };
